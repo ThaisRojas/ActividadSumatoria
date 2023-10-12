046c4c45cbbb593a39ccb65740220fd3c1cf9969
Thais Rojas <thais.rojas@udla.edu.ec>
2023-10-12 16:46:53 -0500

    fin

--- ejercicio.c
+++ ejercicio.c
@@ -3,13 +3,17 @@
 int main() {
     int numero, suma = 0;
 
-printf("Ingrese un número entero: ");
+    printf("Ingrese un numero entero: ");
     scanf("%d", &numero);
 
-if (numero < 1) {
+    if (numero < 1) {
         printf("El número debe ser positivo.\n");
-} else {
-     for (int i = 1; i <= numero; i++) {
-        suma += i;
+    } else {
+        for (int i = 1; i <= numero; i++) {
+            suma += i;
         }
+        printf("La suma de Gauss para %d es: %d\n", numero, suma);
+    }
+
+    return 0;
 }
