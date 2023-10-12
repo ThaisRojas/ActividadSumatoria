e75ebe8048a00a928583a5a2147cafe83742be66
Thais Rojas <thais.rojas@udla.edu.ec>
2023-10-12 16:42:20 -0500

    Calcula

--- ejercicio.c
+++ ejercicio.c
@@ -8,5 +8,8 @@ printf("Ingrese un número entero: ");
 
 if (numero < 1) {
         printf("El número debe ser positivo.\n");
-} 
+} else {
+     for (int i = 1; i <= numero; i++) {
+        suma += i;
+        }
 }
