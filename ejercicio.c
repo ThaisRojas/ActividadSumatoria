14669ee1533f132138000b7c92bd3ab0a49ba159
Thais Rojas <thais.rojas@udla.edu.ec>
2023-10-12 16:38:28 -0500

    Verifica que sea positivo

---
+++ ejercicio.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int numero, suma = 0;
+
+printf("Ingrese un número entero: ");
+    scanf("%d", &numero);
+
+if (numero < 1) {
+        printf("El número debe ser positivo.\n");
+} 
+}
